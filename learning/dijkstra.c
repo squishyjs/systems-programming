23dff4bc2a766d555cfd46e1ec5faf8d3a3534c6
john <johnsciuto3@gmail.com>
2025-04-24 20:22:14 +1000

    Adding Dijkstra's algorithm in C

---
+++ learning/dijkstra.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define P_INFINITY  9999
+#define N_INFINITY  -9999
+
+int main(void) {
+
+    printf("%d\n", P_INFINITY);
+    printf("%d\n", N_INFINITY);
+
+    return 0;
+}
