56173baf760e118fb06f48e48ab657543a9659e5
john <johnsciuto3@gmail.com>
2025-04-27 13:27:11 +1000

    project-based learning for C

---
+++ projects/depth-first-search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+
+struct Graph {
+
+};
+
+typedef struct Vertex {
+
+} Vertex;
+
+
+void depth_fist_search(struct Graph, Vertex) {
+    sleep(1);
+    puts("Starting DFS algorithm..");
+}
+
+int main(int argc, char *argv[]) { return 0; }
