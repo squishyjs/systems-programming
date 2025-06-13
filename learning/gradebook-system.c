7112248372f7d7aeeccf69bf3cc7e76f322d2d25
john <johnsciuto3@gmail.com>
2025-06-13 10:23:36 +1000

    Gradebook Database progress

--- learning/gradebook-system.c
+++ learning/gradebook-system.c
@@ -3,11 +3,21 @@
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define F                   fflush(stdout)
 #define MAX_NAME_LEN        100
 #define MAX_STUDENTS        1000
 #define NUM_SUBJECTS        5
+#define VIEWING             true     
+
+enum DatabaseMenu {
+    ADD_STUDENT=1,
+    VIEW_ALL,
+    SEARCH_STUDENT,
+    SORT_STUDENTS,
+    SAVE_AND_EXIT
+};
 
 typedef struct Student {
     char *name;
@@ -23,7 +33,6 @@ typedef struct GradeBook {
 } GradeBook;
 
 void print_student(Student student) {
-    //FIXME: function not calculating the grades correctly
     printf("\nStudent name: %s\n", student.name);
     printf("Student id: %d\n", student.studentID);
 
@@ -37,6 +46,13 @@ void print_student(Student student) {
     }
 
     printf("Grade Average: %.2f\n", student.average);
+}
+
+bool invalid_grade(float grade) {
+    if (grade < 0 || grade > 100) {
+        return true;
+    }
+    return false;
 }
 
 Student new_student() {
@@ -66,6 +82,16 @@ Student new_student() {
             fprintf(stderr, "Something went wrong. Invalid grade input.\n");
             exit(EXIT_FAILURE);
         }
+        grade = new_student.grades[i];
+        while (invalid_grade(grade)) {
+            printf("Please enter a grade between (0-100).\n");
+            printf("Grade %d: ", i + 1);
+            if (scanf("%f", &new_student.grades[i]) != 1) {
+                fprintf(stderr, "Something went wrong. Invalid grade input.\n");
+                exit(EXIT_FAILURE);
+            }
+            grade = new_student.grades[i];
+        }
 
         average += new_student.grades[i];
     }
@@ -80,7 +106,67 @@ Student new_student() {
 
 void clean_book(GradeBook *gb) {
     for (int i = 0; i < gb->count; ++i) {
-        free(gb->students[i].name); // free strdup names
+        // free strdup malloc
+        free(gb->students[i].name);
+    }
+}
+
+void add_student() {
+    // TODO
+}
+
+void view_all_students() {
+    // TODO
+}
+
+void search_student() {
+    // TODO
+}
+
+void sort_students() {
+    // TODO
+}
+
+void save_exit() {
+    // TODO
+}
+
+void launch_student_database() {
+    printf("\nWelcome to the Persistent Student Database!\n");
+    bool BROWSING = true;
+
+    printf("1. Add Student\n");
+    printf("2. View All Students\n");
+    printf("3. Search Student (by name or ID)\n");
+    printf("4. Sort Students (by average or name)\n");
+    printf("5. Save & Exit\n\n");
+    printf("> ");
+
+    int option;
+    if (scanf("%d", &option) != 1) {
+        fprintf(stderr, "Something went wrong. Invalid grade input.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    switch (option) {
+        case ADD_STUDENT:
+            add_student();
+            break;
+        case VIEW_ALL:
+            view_all_students();
+            break;
+        case SEARCH_STUDENT:
+            search_student();
+            break;
+        case SORT_STUDENTS:
+            sort_students();
+            break;
+        case SAVE_AND_EXIT:
+            save_exit();
+            break;
+        default:
+            printf("Please enter (1-5).");
+            break;
     }
 }
 
@@ -88,7 +174,7 @@ int main(void) {
     struct GradeBook class_grades;
     class_grades.count = 0;
     class_grades.student_info = print_student; // function pointer
-    
+
     int num_students = 3;
     printf("Gradebook System: Enter info for %d students.\n", num_students);
 
@@ -100,15 +186,19 @@ int main(void) {
     }
 
     float class_total = 0;
+    printf("\n======= Summary =======");
     for (int i = 0; i < class_grades.count; ++i) {
         class_grades.student_info(class_grades.students[i]);
         class_total += class_grades.students[i].average;
     }
 
     printf("\nClass Average: %.2f\n", class_total / class_grades.count);
+    printf("\n=======================");
 
-    // memory free
+    // free memory
     clean_book(&class_grades);
+    
+    // launch_student_database();
 
     return EXIT_SUCCESS;
 }
